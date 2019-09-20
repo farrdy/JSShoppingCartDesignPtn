2a82edff1d73e9ef8873a4fff2c586ec7b77e922
Farai <farai.motsi@gmail.com>
2019-09-20 18:06:21 +0200

    added remove from localstorage function

--- js/app.js
+++ js/app.js
@@ -1,92 +1,125 @@
-const courses = document.querySelector('#courses-list'),
-    shoppingCartContent = document.querySelector('#cart-content tbody'),
-    clearCartBtn = document.querySelector('#clear-cart');
+const courses = document.querySelector("#courses-list"),
+  shoppingCartContent = document.querySelector("#cart-content tbody"),
+  clearCartBtn = document.querySelector("#clear-cart");
 
 loadEventListeners();
 
 function loadEventListeners() {
-    courses.addEventListener('click', buyCourse);
-    shoppingCartContent.addEventListener('click', removeCourse);
-    clearCartBtn.addEventListener('click', clearCart);
+  courses.addEventListener("click", buyCourse);
+  shoppingCartContent.addEventListener("click", removeCourse);
+  clearCartBtn.addEventListener("click", clearCart);
+  document.addEventListener("DOMContentLoaded", getFromLocalStorage);
 }
 
 function buyCourse(e) {
-    e.preventDefault();
-    console.log(e.target);
-    alert('course')
-    if (e.target.classList.contains('add-to-cart')) {
-        const course = e.target.parentElement.parentElement;
-        getCourseInfo(course);
-    }
+  e.preventDefault();
+  if (e.target.classList.contains("add-to-cart")) {
+    const course = e.target.parentElement.parentElement;
+    getCourseInfo(course);
+  }
 }
 
 function getCourseInfo(course) {
+  const courseInfo = {
+    image: course.querySelector("img").src,
+    title: course.querySelector("h4").textContent,
+    price: course.querySelector(".price span").textContent,
+    id: course.querySelector("a").getAttribute("data-id")
+  };
 
-    const courseInfo = {
-        image: course.querySelector('img').src,
-        title: course.querySelector('h4').textContent,
-        price: course.querySelector('.price span').textContent,
-        id: course.querySelector('a').getAttribute('data-id')
-    }
-
-    addIntoCart(courseInfo);
-    //console.log(courseInfo);
+  addIntoCart(courseInfo);
+  //console.log(courseInfo);
 }
 //insrt into shopping cart
 
 function addIntoCart(course) {
-
-    const row = document.createElement('tr');
-    row.innerHTML = `
+  const row = document.createElement("tr");
+  row.innerHTML = `
     <tr>
     <td> <img src="${course.image}"></td>
     <td>${course.title}</td>
     <td>${course.price}</td>
     <td><a href ="#" class="remove" data-id="${course.id}">X</td>
     </tr>
-    `
-    //add into the shoppingcart
-    shoppingCartContent.appendChild(row);
-    saveIntoStorage(course);
+    `;
+  //add into the shoppingcart
+  shoppingCartContent.appendChild(row);
+  saveIntoStorage(course);
 }
 
 //add courss into localstorage
 function saveIntoStorage(course) {
-    let courses = getCoursesFromStorage();
+  let courses = getCoursesFromStorage();
 
-    courses.push(course);
-    //since storage only saves strings 
-    localStorage.setItem('courses', JSON.stringify(courses));
-
+  courses.push(course);
+  //since storage only saves strings
+  localStorage.setItem("courses", JSON.stringify(courses));
 }
 
 //Get contents from local storage
 function getCoursesFromStorage() {
+  let courses;
 
-    let courses;
-
-    if (localStorage.getItem('courses') === null) {
-        courses = [];
-    }
-    else {
-
-        courses = JSON.parse(localStorage.getItem('courses'));
-    }
-    return courses;
+  if (localStorage.getItem("courses") === null) {
+    courses = [];
+  } else {
+    courses = JSON.parse(localStorage.getItem("courses"));
+  }
+  return courses;
 }
 
 function removeCourse(e) {
+  let course, courseId;
+  if (e.target.classList.contains("remove")) {
+    course = e.target.parentElement.parentElement;
+    courseId = course.querySelector("a").getAttribute("data-id");
+    e.target.parentElement.parentElement.remove();
+  }
 
-    if (e.target.classList.contains('remove')) {
-
-        e.target.parentElement.parentElement.remove();
-
+  removeCourseLocalStorage(courseId);
+}
+//remove course from local storage
+function removeCourseLocalStorage(id) {
+  let coursesLS = getCoursesFromStorage();
+  coursesLS.forEach(function(course_ls, index) {
+    if (course_ls.id === id) {
+      coursesLS.splice(index, 1);
     }
+  });
+
+  localStorage.setItem("courses", JSON.stringify(coursesLS));
 }
 
 function clearCart() {
-
-    //shoppingCartContent.innerHTML = '';
+  //shoppingCartContent.innerHTML = '';
+  while (shoppingCartContent.firstChild) {
     shoppingCartContent.removeChild(shoppingCartContent.firstChild);
+  }
+  clearLocalStorage();
 }
 
+function clearLocalStorage() {
+  localStorage.clear();
+}
+
+//Loads from Local storage
+
+function getFromLocalStorage() {
+  let coursesLS = getCoursesFromStorage();
+  //LOOP THROUGH COURSES AND PRINT INTIO CART
+  coursesLS.forEach(function(course) {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+                        <tr>
+                        <td> <img src="${course.image}"></td>
+                        <td>${course.title}</td>
+                        <td>${course.price}</td>
+                        <td><a href ="#" class="remove" data-id="${
+                          course.id
+                        }">X</td>
+                    </tr>
+                    `;
+    shoppingCartContent.appendChild(row);
+  });
+}
